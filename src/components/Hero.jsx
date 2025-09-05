598cdf045593e62c60550e9c82f8da0afed449e3
polysola <polyseedify@gmail.com>
2025-09-05 21:57:09 +0700

    fux

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -68,9 +68,9 @@ const Hero = () => {
         id="cta"
         className="flex flex-col items-center opacity-0"
       >
-        <a href="https://app.ston.fi/swap?chartVisible=true&ft=TON&tt=EQB1-yL4Gx2B5M76P8aggdR2UJS6X5sS_6680S15m7jImAwY&chartInterval=1w" className="btn">Buy</a>
+        <a href="https://app.ston.fi/swap?chartVisible=true&ft=TON&tt=TBA&chartInterval=1w" className="btn">Buy</a>
         <a style={{ marginTop: '0rem', marginBottom: '1rem' }} className="mt-0 btn" href="https://t.me/iTon_TONMobile" >Telegram</a>
-            <p className="font-normal text-xl">CA: EQB1-yL4Gx2B5M76P8aggdR2UJS6X5sS_6680S15m7jImAwY</p>
+            <p className="font-normal text-xl">CA: TBA</p>
         <p className="font-normal text-xl">From ston.fi</p>
       </div>
     </section>
